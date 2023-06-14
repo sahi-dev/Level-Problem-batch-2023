a6469cafdbe475b6794d0d63caff47959f079f95
Sahil Sah <sahilsah357@gmail.com>
2023-06-14 13:43:27 +0530

    fourth commit

---
+++ level2/program4.c
@@ -0,0 +1,8 @@
+/*Write a function to accept an integer (32 bits) and operation type to do the following and return the result to main function.
+    - Set 2 bits from nth bit position, if operation type is 1
+    - Clear 3 bits from nth bit position, if operation type is 2
+    - Toggle MSB, if operation type is 3
+   You can consider the below function prototype,
+    - bit_operations(int num, int oper_type, int pos);*/
+
+    
---
+++ level2/program5.c
@@ -0,0 +1,8 @@
+/*Write a function to find the type of the character passed
+    - 'A' to 'Z', return 1
+    - 'a; to 'z', return 2
+    - '0' to '9', return 3
+    - Any Other printable symbols, return 4
+    - Non printable symbols, return 5 */
+
+    
